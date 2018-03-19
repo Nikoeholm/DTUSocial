210455f7244a22724183039cc8f057daa2cf1238
Swagam <s165155@student.dtu.dk>
2018-03-19 16:35:03 +0100

    Able to chat locally.

--- src/app/main/container/chat/chat.component.ts
+++ src/app/main/container/chat/chat.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit{
+  @ViewChild('messageInput') messageInputRef: ElementRef;
+
+  messages = [];
+
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  onSendMessage(){
+
+    const ingMessage = this.messageInputRef.nativeElement.value;
+    this.messages.push(ingMessage);
+    this.messageInputRef.nativeElement.value = "";
+
+}
+
+
 
 }
