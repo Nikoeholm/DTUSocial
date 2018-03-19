cee82ae8127801114b5ac1508078006da21c426e
Swagam <s165155@student.dtu.dk>
2018-03-19 15:06:04 +0100

    Chat template initiated & Contacts template initiated.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,18 +4,21 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material';
 
 @NgModule({
   imports: [BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
     MatMenuModule,
-    MatIconModule],
+    MatIconModule,
+    MatSidenavModule],
   exports: [BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
     MatMenuModule,
-    MatIconModule]
+    MatIconModule,
+    MatSidenavModule]
 })
 export class MaterialModule { }
 
