44f0d877ce7dcb43ea347503277a58bc64d64f16
Khurram Saeed Malik <s165162@student.dtu.dk>
2018-04-09 11:18:40 +0200

    Refactored material module typescript class

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import {MatToolbarModule} from '@angular/material/toolbar';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
-import {MatSidenavModule} from '@angular/material';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule, MatListModule} from '@angular/material';
+import {MatInputModule,
+  MatListModule,
+  MatCardModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatMenuModule,
+  MatButtonModule,
+  MatToolbarModule} from '@angular/material';
 
 @NgModule({
   imports: [BrowserAnimationsModule,
