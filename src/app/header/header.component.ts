7791be499f1dbb1a6e27154849dab719fb0036cb
Khurram Saeed Malik <s165162@student.dtu.dk>
2018-04-10 09:27:52 +0200

    Navigate to /login on logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -18,7 +18,7 @@ export class HeaderComponent implements OnInit {
   logout() {
     console.log('Logged out!');
     this.loginService.logout();
-    this.router.navigate(['/']);
+    this.router.navigate(['/login']);
   }
 
 }
